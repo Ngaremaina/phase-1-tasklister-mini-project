0af8582df9087b7f4fd62511bd567e2ff704660a
Ngaremaina <ngaremaina4@gmail.com>
2023-03-23 13:05:19 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let task_input = document.getElementById('new-task-description').value
+    handleToDo(task_input)
+    form.reset()
+  })
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  p.textContent = `${todo}`
+  document.querySelector('ul').appendChild(p)
+
+  let btn = document.createElement('button')
+  btn.textContent = " X"
+  p.appendChild(btn)
+
+  btn.addEventListener('click', handleDelete)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
